19f16018df8f99a1908f80051fe241e8401c2957
felix_zhao <zhaobowen9876@gmail.com>
2021-06-29 09:31:34 +0800

    upload project files

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/global/router'
@@ -10,8 +12,11 @@ Vue.prototype.$cache = cache
 
 Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  components: { App },
+  template: '<App/>'
+})
